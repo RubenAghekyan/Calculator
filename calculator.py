b7ad3ae3aa57c085b975d8df5240d6667d412ac9
RubenAghekyan <rubaghekyan@gmail.com>
2022-02-21 12:50:48 +0400

    new-feature

---
+++ calculator.py
@@ -0,0 +1,19 @@
+def calculator():
+    numb1 = int(input("Please write your first number:\n "))
+    numb2 = int(input("Please write your second number:\n "))
+    command = input("Please write what do you want to do(-,+):\n ")
+    if command == "-":
+        return numb1 - numb2
+    elif command == "+":
+        return numb1 + numb2
+    elif command == "/":
+        return numb1 / numb2
+    elif command == "*":
+        return numb1 * numb2
+    else:
+        print("=================================================")
+        print("Please write the required command: ")
+        return calculator()
+
+
+print(calculator())
